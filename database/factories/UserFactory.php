8769bdc5eaf2c40c6b747f5232d5c0a3a7cb87eb
Cesar Escudero <cedaesca@gmail.com>
2024-01-20 09:26:06 -0400

    chore: remove unused fields from factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,19 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }

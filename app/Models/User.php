39fa4059f3e4b8e6ba3ac07ca8433d3759d15483
César Escudero <cedaesca@gmail.com>
2024-01-20 09:20:20 -0400

    chore: fresh install code cleanup (#1)
    * chore: remove unused tables and views
    
    * chore: ignore sanctum migrations
    
    * chore: remove unused user attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,8 +38,5 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected $casts = [];
 }

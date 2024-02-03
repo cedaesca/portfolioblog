d0aad2df1da85f5a174f16d3178f34e3b861bc68
Cesar Escudero <cedaesca@gmail.com>
2024-02-03 09:10:26 -0400

    test(factory): add Tag factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'css_background_color' => $this->faker->colorName,
+            'css_text_color' => $this->faker->colorName
+        ];
+    }
+}

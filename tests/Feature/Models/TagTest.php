456154c87e16cbc1e0a92e9aac2f24c4c3b63287
Cesar Escudero <cedaesca@gmail.com>
2024-02-03 09:50:18 -0400

    tests(tag): adding test layout

---
+++ tests/Feature/Models/TagTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TagTest extends TestCase
+{
+    /** @test */
+    public function a_tag_belongs_to_a_post()
+    {
+        //
+    }
+}

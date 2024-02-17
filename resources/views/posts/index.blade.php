e2ee30c6275196cdcea5e1b5f2490c43eb6cf220
Cesar Escudero <cedaesca@gmail.com>
2024-02-17 09:52:05 -0400

    feat: improve semantics and add links

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{{ config('app.name') }}</title>
-    </head>
-    <body>
-        <h1>Projects</h1>
-        <ul>
-            @foreach($projects as $project)
-                <li>{{ $project->title }}</li>
-            @endforeach
-        </ul>
-        <br />
-        <h1>Articles</h1>
-        <ul>
-            @foreach($articles as $article)
-                <li>{{ $article->title }}</li>
-            @endforeach
-        </ul>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <main>
+        <section>
+            <h1>Projects</h1>
+            <ul>
+                @foreach($projects as $project)
+                    <li><a href="{{ route('posts.show', $project->slug) }}">{{ $project->title }}</a></li>
+                @endforeach
+            </ul>
+        </section>
+        
+        <section>
+            <h1>Articles</h1>
+            <ul>
+                @foreach($articles as $article)
+                    <li><a href="{{ route('posts.show', $article->slug) }}">{{ $article->title }}</a></li>
+                @endforeach
+            </ul>
+        </section>
+    </main>
+</body>
 </html>
